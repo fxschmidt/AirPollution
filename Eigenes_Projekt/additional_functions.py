cd2d510d648da9bec9eb182c41df23c3eecdd8d0
Felix Schmidt <felixsch00@outlook.de>
2022-12-29 16:20:45 +0100

    Arbeit am 29.12

--- Eigenes_Projekt/additional_functions.py
+++ Eigenes_Projekt/additional_functions.py
@@ -5,100 +5,80 @@ import json
 from time import sleep
 import requests as requests
 
-def calc_aqi(df,whitout_pm10=True):
-    """Calulate the AirQualityIndex for the five key pollutants (poorest level counts). 
-    For more see the Dokumentation Folder. The concentrations should be in micro g/m3
+def calc_aqi(value,pollutant,return_int=True):
 
-    Args:
-        df (pd.DataFrame): DataFrame which contain the pollutants
-        without_pm10 (Noolean): If the df contains no pm10 column
-    Return:
-        int: 0 to 5 which represents the quality levels
-        list of str: pollutant which is too high
-    """
-    if whitout_pm10:
-        pm2_5,no2,o3,so2 = df[["mean_pm2_5","max_value_no2","max_value_o3","max_value_so2"]]
-        #set zero because there is no data for pm10
-        pm10=0
-    else:
-        pm2_5,pm10,no2,o3,so2 = df[["mean_pm2_5","mean_pm10","max_value_no2","max_value_o3","max_value_so2"]]
-    #classify pm2.5
-    if pm2_5<10:
-        pm2_5=0
-    elif pm2_5<20:
-        pm2_5=1
-    elif pm2_5<25:
-        pm2_5=2
-    elif pm2_5<50:
-        pm2_5=3
-    elif pm2_5<75:
-        pm2_5=4
-    else:
-        pm2_5=5
-        
-    if pm10<20:
-        pm10=0
-    elif pm10<40:
-        pm10=1
-    elif pm10<50:
-        pm10=2
-    elif pm10<100:
-        pm10=3
-    elif pm10<150:
-        pm10=4
-    else:
-        pm10=5
-        
-    if no2<40:
-        no2=0
-    elif no2<90:
-        no2=1
-    elif no2<120:
-        no2=2
-    elif no2<230:
-        no2=3
-    elif no2<340:
-        no2=4
-    else:
-        no2=5
-        
-    if o3<50:
-        o3=0
-    elif o3<100:
-        o3=1
-    elif o3<130:
-        o3=2
-    elif o3<240:
-        o3=3
-    elif o3<380:
-        o3=4
-    else:
-        o3=5    
-        
-    if so2<100:
-        so2=0
-    elif so2<200:
-        so2=1
-    elif so2<350:
-        so2=2
-    elif so2<500:
-        so2=3
-    elif so2<750:
-        so2=4
-    else:
-        so2=5
+    if pollutant=="pm2_5":
+        if pm2_5<10:
+            pm2_5=0
+        elif pm2_5<20:
+            pm2_5=1
+        elif pm2_5<25:
+            pm2_5=2
+        elif pm2_5<50:
+            pm2_5=3
+        elif pm2_5<75:
+            pm2_5=4
+        else:
+            pm2_5=5
+    
+    if pollutant=="pm10":
+        if pm10<20:
+            pm10=0
+        elif pm10<40:
+            pm10=1
+        elif pm10<50:
+            pm10=2
+        elif pm10<100:
+            pm10=3
+        elif pm10<150:
+            pm10=4
+        else:
+            pm10=5
+    
+    if pollutant=="no2":    
+        if no2<40:
+            no2=0
+        elif no2<90:
+            no2=1
+        elif no2<120:
+            no2=2
+        elif no2<230:
+            no2=3
+        elif no2<340:
+            no2=4
+        else:
+            no2=5
+    
+    if pollutant=="o3":    
+        if o3<50:
+            o3=0
+        elif o3<100:
+            o3=1
+        elif o3<130:
+            o3=2
+        elif o3<240:
+            o3=3
+        elif o3<380:
+            o3=4
+        else:
+            o3=5    
+    
+    if pollutant=="so2":    
+        if so2<100:
+            so2=0
+        elif so2<200:
+            so2=1
+        elif so2<350:
+            so2=2
+        elif so2<500:
+            so2=3
+        elif so2<750:
+            so2=4
+        else:
+            so2=5
             
-    # choose the highest AQI Value
-    pollutants = ["pm2_5","pm10","o3","no2","so2"]
-    aqi_values = np.array([pm2_5,pm10,o3,no2,so2])
-    aqi_max= np.max(aqi_values)
-    # which value is too high, return the pollutant only if level is above fair
-    if aqi_max>=1:
-        aqi_max_idx = np.argwhere(aqi_max == aqi_max)
-        aqi_max_pollutant = [pollutants[i] for i in aqi_max_idx.flatten().tolist()]
-    else:
-        aqi_max_pollutant=[]
-    return aqi_max,aqi_max_pollutant
+    if return_int:
+        return 
     
 def get_epa_data(param: int,year: int,site: int,folder: str,check_exists=True,only_get_path=False ):
     """Get the data from one of the sites in LA on a daily base
